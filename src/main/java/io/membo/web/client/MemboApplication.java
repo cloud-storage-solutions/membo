b11f3a5e48a2de48f080404e18245128f95af551
Lyuboslav Kanev <lubokkanev@gmail.com>
2018-07-21 03:03:16 +0300

    Don't use spring for dependency injection

--- src/main/java/io/membo/web/client/MemboApplication.java
+++ src/main/java/io/membo/web/client/MemboApplication.java
@@ -1,9 +1,16 @@
 package io.membo.web.client;
 
+import io.membo.web.client.convert.RssEntryToPostConverter;
+import io.membo.web.client.convert.RssEntryToRedditPostConverter;
 import io.membo.web.client.crosspost.RedditToMemoCrossposter;
+import io.membo.web.client.crosspost.RedditUrlShortener;
+import io.membo.web.client.crosspost.submit.memo.MemoSubmitter;
 import io.membo.web.client.rss.RssFetchingException;
+import io.membo.web.client.rss.reddit.BtcRssFetcher;
 import io.membo.web.client.transaction.broadcast.TransactionBroadcastException;
+import io.membo.web.client.transaction.broadcast.bitbox.BitboxTransactionBroadcaster;
 import io.membo.web.client.transaction.create.TransactionCreationException;
+import io.membo.web.client.transaction.create.memo.ProcessMemoTransactionCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,15 +21,14 @@ import javax.annotation.PostConstruct;
 @EnableJpaRepositories("io.membo.repositories")
 @SpringBootApplication
 public class MemboApplication {
-    @Autowired
-    private RedditToMemoCrossposter crossposter;
-
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(MemboApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() throws InterruptedException, TransactionCreationException, RssFetchingException, TransactionBroadcastException {
-        crossposter.crosspostAllForever(30);
+        new RedditToMemoCrossposter(
+                new MemoSubmitter(
+                        new ProcessMemoTransactionCreator(),
+                        new BitboxTransactionBroadcaster()),
+                new BtcRssFetcher(
+                        new RssEntryToRedditPostConverter(
+                                new RedditUrlShortener())))
+                .crosspostAllForever(1);
     }
 }
