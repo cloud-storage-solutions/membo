ee686e346cc690257ed12d4fc293e7cfeeb59750
Lyuboslav Kanev <lkanev@vmware.com>
2019-05-08 11:26:23 +0300

    Fix bitdb lookup error - doesn't work with bitcoincash: prefix

--- src/main/java/io/membo/Account.java
+++ src/main/java/io/membo/Account.java
@@ -1,6 +1,6 @@
 package io.membo;
 
 public class Account {
-   public static final String publicKey = "bitcoincash:qqdgw5am97nz6utgltnsqhql063apwv48vxec778hn";
    public static final String privateKey = ""; // TODO: don't submit this!
+   public static final String publicKey = "qqdgw5am97nz6utgltnsqhql063apwv48vxec778hn";
 }
